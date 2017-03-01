13ba249d5b579c33a46f556db7df69efc6d589b0
JuanCarlosGI <juancarlos.gi94@gmail.com>
2017-03-01 17:29:11 -0600

    Differentiate between Debug and Release input types
    In debug mode, file path will be specified in an input, while in release
    mode, file path will be specified as a command line argument.

--- Compiler/Program.cs
+++ Compiler/Program.cs
@@ -8,19 +8,28 @@
     {
         static void Main(string[] args)
         {
-            Console.Write("Dirección del archivo: ");
-            var path = Console.ReadLine();
+#if DEBUG
+            Console.Write("File address: ");
+            args = new[] { Console.ReadLine() };
+#endif
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Please specify a file address.");
+                return;
+            }
+
+            var path = args[0];
 
             if (File.Exists(path))
             {
                 var scanner = new Scanner(path);
                 var parser = new Parser(scanner);
                 parser.Parse();
-                Console.WriteLine(parser.errors.count + " errors detected");
+                Console.WriteLine(parser.errors.count + " errors detected.");
             }
             else
             {
-                Console.WriteLine("Dirección inválida");
+                Console.WriteLine("File not found.");
             }
         }
     }
